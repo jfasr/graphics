9fd8c1b45383f26ce3e7715739bf865b534479db
João Filipe <99099086+jfasr@users.noreply.github.com>
2023-12-28 11:48:28 -0300

    tarefa concluída

--- pi/rast-1.cpp
+++ pi/rast-1.cpp
@@ -12,25 +12,6 @@ struct {
 	int *matrizImagem;
 } ppmImage;
 
-int* getTamanhoImagem (std::string str, int*vetor){
-	std::string stringTemp;
-	std::stringstream ss(str);
-
-
-	getline(ss, stringTemp,' ');
-	vetor[0] = std::stoi(stringTemp);
-	getline(ss, stringTemp,' ');
-	vetor[1] = std::stoi(stringTemp);
-
-	return vetor;
-}
-
-
-/* PRECISO CRIAR UMA FORMA DE PEGAR OS PIXELS DA IMAGEM LIDA 
-E ARMAZENAR NA NOSSA MATRIZ CRIADA 
-função acima pode ser uma forma de como fazer...
-*/
-
 
 int main (int argc, char* argv[]){
 	std::ifstream imagemPPM;
@@ -39,25 +20,60 @@ int main (int argc, char* argv[]){
 	
 	//tendo certeza que o arquivo foi aberto corretamente
 	if (imagemPPM.is_open()){
+		//Armazeno o tipo da imagem
+		std::string tipoImagem;
+		imagemPPM >> tipoImagem;
+		//Armazeno o tamanho da imagem
+		std::string colunas, linhas;
+		imagemPPM >> colunas;
+		imagemPPM >> linhas;
+		//Armazeno o valor maximo de tom
+		std::string valorMaximo;
+		imagemPPM >> valorMaximo;
+		//Transformo esses valores em inteiros, para criar a matriz dos pixels
+		int x, y;
+		x = std::stoi(colunas);
+		y = std::stoi(linhas);
+		int matrixPixel[y][x]; 
+		// no arquivo ppm é coluna - linha, pra colcoar na matrix invertemos
+		for (int i = 0; i<y; i++){
+			for (int j = 0; j<x; j++){
+				std::string strTemp;
+				imagemPPM >> strTemp;
+				matrixPixel[i][j] = std::stoi(strTemp);
+			}
+		}
 
-		std::string tipoDaImagem; //primeira linha
-		std::getline(imagemPPM, tipoDaImagem);
+		//crio o novo arquivo
+		std::ofstream novaImagem;
+		novaImagem.open("novaImagem.ppm");
 
-		std::string tamanhoDaImagem; //segunda linha
-		std::getline(imagemPPM, tamanhoDaImagem); // peguei a dupla width e height (precisa ser separado)
+		//crio as variáveis pra essa imagem
+		std::string tipoNI, colunasNI, linhasNI, valorMaximoNI;
 		
-		// nesse vetor é armazenado o numero de colunas e linhas da nossa matriz(imagem)
-		int vetorTamanho[2];
-		getTamanhoImagem(tamanhoDaImagem, vetorTamanho);
+		tipoNI.assign(tipoImagem);
+		colunasNI.assign(colunas);
+		linhasNI.assign(linhas);
+		valorMaximoNI.assign(valorMaximo);
 
-		std::string valorMaxCor;
-		std::getline(imagemPPM,valorMaxCor);
-		int valorCor = std::stoi(valorMaxCor);
 
-		//criada a matriz que vai armazenar os pixels da nossa imagem
-		int matrizTeste[vetorTamanho[0]][vetorTamanho[1]];
-	}
+		//escrevo no novo arquivo
+
+		novaImagem << tipoNI << std::endl;
+		novaImagem << colunasNI << " " << linhasNI << std::endl;
+		novaImagem << valorMaximoNI << std::endl;
+
+		// escrevo os pixels
+
+		for (int i = 0; i<y; i++){
+			for (int j = 0; j<x; j++){
+				std::string pixelTemp;
+				pixelTemp = std::to_string(matrixPixel[i][j]);
+				novaImagem << pixelTemp << std::endl;
+			}
+		}
 
 	imagemPPM.close();
 	return 0;
+	}
 }
